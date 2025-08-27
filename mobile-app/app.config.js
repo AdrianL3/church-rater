dbc8cb6aaa5b1e444c7c9907bfd54f45f358c252
Adrian Laksana <adrianjlaksana@gmail.com>
2025-08-27 16:27:15 -0700

    prepping for deployment, final save before

--- mobile-app/app.config.js
+++ mobile-app/app.config.js
@@ -3,8 +3,8 @@ import 'dotenv/config';
 
 export default {
   expo: {
-    name: 'mobile-app',
-    slug: 'mobile-app',
+    name: 'churchfinder',
+    slug: 'church-finder',
     version: '1.0.0',
     orientation: 'portrait',
     icon: './assets/images/icon.png',
@@ -13,6 +13,14 @@ export default {
     newArchEnabled: true,
     ios: {
       supportsTablet: true,
+      bundleIdentifier: 'com.adrianl3.churchfinder',
+      buildNumber: '1',
+      infoPlist: {
+        NSLocationWhenInUseUsageDescription:
+          'We use your location to show nearby churches',
+        NSPhotoLibraryAddUsageDescription:
+          'Allow adding photos to church notes.',
+      },
     },
     android: {
       adaptiveIcon: {
@@ -37,22 +45,17 @@ export default {
           backgroundColor: '#ffffff',
         },
       ],
+      'expo-location',
     ],
     extra: {
+      apiUrl: 'https://tf95p1362f.execute-api.us-west-2.amazonaws.com',
+      cognitoRegion: 'us-west-1',
+      cognitoUserPoolId: 'us-west-1_NNGeSDxTP',
+      cognitoAppClientId: '32bvrqma8qafn20tj6ki325grc',
       googleMapsApiKey: process.env.GOOGLE_API_KEY,
     },
     experiments: {
       typedRoutes: true,
-    },
-    extra: {
-      googleMapsApiKey: process.env.GOOGLE_API_KEY,
-    },
-    extra: {
-      apiUrl: "https://tf95p1362f.execute-api.us-west-2.amazonaws.com",
-      cognitoRegion: "us-west-1",
-      cognitoUserPoolId: "us-west-1_NNGeSDxTP",
-      cognitoAppClientId: "32bvrqma8qafn20tj6ki325grc",
-      googleMapsApiKey: process.env.GOOGLE_API_KEY,
     },
   },
 };
